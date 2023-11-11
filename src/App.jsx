9bd663a0ff9eb76b20c027879c84f9a753ab71ac
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-11-11 09:02:08 +0100

    added search jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './components/Homepage'
 import Shop from './components/Shop'
+import Login from './components/Login'
+import Register from './components/Register'
+import SingleProduct from './components/SingleProduct'
 
 function App() {
 
 
   return (
     <>
-        <Routes>
+       <Routes>
           <Route path="/home" element={<Homepage />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/products/single" element={<SingleProduct />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
 
     </>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,44 +1,42 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import Navigation from '../elements/Navigation';
+import Info from '../elements/Info';
+import Footer from '../elements/Footer';
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
 
-const [products, setProducts] = useState([])
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/products");
+      console.log(response.data);
 
-const fetchProducts = async () => {
-    try{
-        const response = await axios.get("http://localhost:3000/api/products")
-        console.log(response.data)
-
-        setProducts(response.data)
-
-    } catch(err){
-        console.log(err)
+      setProducts(response.data);
+    } catch (err) {
+      console.log(err);
     }
-}
+  };
 
-useEffect(()=> {
-    fetchProducts()
-}, [])
-
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <>
-    <div>Shop</div>
-    {products.map((product, index) => {
-        return(
-            <>
+      <Navigation />
+      {products.map((product, index) => {
+        return (
+          <>
             <p key={index}>{product.title}</p>
             <img src={product.image} alt={product.title} />
-            </>
-        )
-    })}
-    
-    
+          </>
+        );
+      })}
+      <Info />
+      <Footer />
     </>
-    
-
-  )
+  );
 }
 
 export default Shop
