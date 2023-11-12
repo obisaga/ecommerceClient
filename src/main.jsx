0ea08139a440ede141b5d3db4af3630db222923d
obisaga <o.bisaga@gmail.com>
2023-11-12 14:56:14 +0100

    simpe routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from './App.jsx'
 // import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import UserProvider from './context/UserContext.jsx'
-import CartProvider from './context/CartContext.jsx';
+// import CartProvider from './context/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <UserProvider>
-      <CartProvider>
+      {/* <CartProvider> */}
         <App />
-      </CartProvider>
+      {/* </CartProvider> */}
     </UserProvider>
   </BrowserRouter>
 );
