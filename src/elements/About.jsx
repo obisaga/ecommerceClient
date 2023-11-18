1502c52834c7fe7065fd1613e4877ca7c5fe96fb
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-11-18 13:06:08 +0100

    adjusted about us section with font and one more br on the text

--- src/elements/About.jsx
+++ src/elements/About.jsx
@@ -7,7 +7,8 @@ const About = () => {
       <section className="about-us">
         <p className="aboutus-text">
         The jewelry shop is a charming boutique that specializes in offering a diverse and exquisite collection of fine jewelry. <br />
-        Whether in search of a bold statement piece or a subtle everyday accessory, the modern jewelry boutique is the ultimate destination for those who value the creativity and innovation of handmade adornments.
+        Whether in search of a bold statement piece or a subtle everyday accessory, <br />
+        the modern jewelry boutique is the ultimate destination for those who value the creativity and innovation of handmade adornments.
         </p>
         <img
           src="https://github.com/obisaga/ecommerceImages/blob/main/free%20stock%20images/pexels-tima-miroshnichenko-6262835.jpg?raw=true"
